e138e063cc3c212877a3f5530809c2f0af7ce771
emmanuel818 <emmanuelg8182001@gmail.com>
2021-06-23 14:38:07 -0500

    Updated scripts page
    Included document.write to display my variable. Still needs coding on the html side.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //pokedex array
 let pokemonList = [
-  {name:'Bulbasaur', height: .7, types: ['grass','poison']},
-  {name:'Charmander', height: .6, types: 'fire'},
-  {name:'Squirtle', height: .5, types: 'water'},  
+  { name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
+  { name: 'Charmander', height: .6, types: 'fire'},
+  { name: 'Squirtle', height: .5, types: 'water'}
 ];
+document.write(pokemonList);
