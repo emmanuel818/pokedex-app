71fd6feea9b748a4cfa0b0656dbe8eb81d294c54
emmanuel818 <emmanuelg8182001@gmail.com>
2021-06-30 14:48:30 -0500

    Updated JS
    added new array and also a function to call on the second array to print to my html page and console.

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,51 @@ let pokemonList = [
     height: .5,
     type: ['water']
   }
-]
+];
 
-for (let i=0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + '  Height:  ' + pokemonList[i].height)
-  console.log(pokemonList[i].name + '  Height:  ' + pokemonList[i].height)
-  if (pokemonList[i].height > 1.0) {
-    document.write('  Wow that is a big Pokemon!')
+let pokemonList2 = [
+  {
+    name:'Bird',
+    height: .7,
+    type: ['grass']
+  },
+  {
+    name: 'Cat',
+    height: 0.6,
+    type: ['fire']
+  },
+  {
+    name: 'Dog',
+    height: .5,
+    type: ['water']
   }
-  document.write('<br>')
-}
+];
+
+// * this loop calls & prints the objects of the above array into the index.html
+// the if condition checks to see if height is larger than 1 if so prints out a statement
+
+// function allows me to print multiple arrays without having to write more code
+function printArrayDetails(list) {
+  for (let i=0; i < list.length; i++) {
+    console.log('<p>' + list[i].name + '  Height:  ' + list[i].height)
+    document.write('<p>' + list[i].name + '  Height: ' + list[i].height)
+    if (list[i].height > 1.0) {
+      console.log ('  Wow that is a big Pokemon!' + '</p>')
+      document.write('  Wow that is a big Pokemon!' + '</p>')
+    };
+  };
+};
+
+// calls or executes function
+printArrayDetails(pokemonList);
+printArrayDetails(pokemonList2);
+
+
+function div(dividend,divisor) {
+  if (divisor === 0){
+    return 'Youre trying to divide by 0.'
+  }else {
+    let result = dividend / divisor;
+    return result;
+  };
+};
