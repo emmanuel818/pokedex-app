863bba37068e020aec9d40a3225556435749b0a0
emmanuel818 <emmanuelg8182001@gmail.com>
2021-07-05 13:46:49 -0500

    Replaced for loop with forEach()  function
    This will clean up my code and still print the the details on the console and the webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,37 @@
 //pokedex array
-let pokemonList = [
-  {
-    name:'Bulbasaur',
-    height: .7,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-  },
-  {
-    name: 'Squirtle',
-    height: .5,
-    type: ['water']
-  }
-];
+  let pokemonList = [
+    {
+      name:'Bulbasaur',
+      height: .7,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ['fire', 'flying']
+    },
+    {
+      name: 'Squirtle',
+      height: .5,
+      type: ['water']
+    }
+  ];
 
-let pokemonList2 = [
-  {
-    name:'Bird',
-    height: .7,
-    type: ['grass']
-  },
-  {
-    name: 'Cat',
-    height: 0.6,
-    type: ['fire']
-  },
-  {
-    name: 'Dog',
-    height: .5,
-    type: ['water']
-  }
-];
+
+
+function myLoopFunction(pokemon) {
+  console.log('<p>' + pokemon.name + ' ' + 'Height: ' + pokemon.height + ' ' + pokemon.type + '<p>');
+  document.write('<p>' + pokemon.name + ' ' + 'Height: ' + pokemon.height + ' ' +  pokemon.type + '<p>');
+}
+pokemonList.forEach(myLoopFunction);
+
+
 
 // * this loop calls & prints the objects of the above array into the index.html
 // the if condition checks to see if height is larger than 1 if so prints out a statement
 
 // function allows me to print multiple arrays without having to write more code
-function printArrayDetails(list) {
+/*function printArrayDetails(list) {
   for (let i=0; i < list.length; i++) {
     console.log('<p>' + list[i].name + '  Height:  ' + list[i].height)
     document.write('<p>' + list[i].name + '  Height: ' + list[i].height)
@@ -49,17 +41,7 @@ function printArrayDetails(list) {
     };
   };
 };
-
+*/
 // calls or executes function
-printArrayDetails(pokemonList);
-printArrayDetails(pokemonList2);
-
-
-function div(dividend,divisor) {
-  if (divisor === 0){
-    return 'Youre trying to divide by 0.'
-  }else {
-    let result = dividend / divisor;
-    return result;
-  };
-};
+//printArrayDetails(pokemonList);
+//printArrayDetails(pokemonList2);
