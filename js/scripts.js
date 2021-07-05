d22d24f6636b56700881beaace2604c56040b406
emmanuel818 <emmanuelg8182001@gmail.com>
2021-07-05 14:58:55 -0500

    Updated script page
    Wrapped my array in an IIFE. Create a new variable to hold what IIFE will return. Adjusted my forEach function to access the variables inside the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-//pokedex array
+//wrapped  my array in an IIFE to avoid accessing the global state.
+let pokemonRepository = (function () {
   let pokemonList = [
     {
       name:'Bulbasaur',
@@ -17,31 +18,24 @@
     }
   ];
 
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
 
 
-function myLoopFunction(pokemon) {
+
+// replaced the for loop with a forEach() function to clean up my code.
+pokemonRepository.getAll().forEach(function(pokemon) {
   console.log('<p>' + pokemon.name + ' ' + 'Height: ' + pokemon.height + ' ' + pokemon.type + '<p>');
   document.write('<p>' + pokemon.name + ' ' + 'Height: ' + pokemon.height + ' ' +  pokemon.type + '<p>');
-}
-pokemonList.forEach(myLoopFunction);
-
-
-
-// * this loop calls & prints the objects of the above array into the index.html
-// the if condition checks to see if height is larger than 1 if so prints out a statement
-
-// function allows me to print multiple arrays without having to write more code
-/*function printArrayDetails(list) {
-  for (let i=0; i < list.length; i++) {
-    console.log('<p>' + list[i].name + '  Height:  ' + list[i].height)
-    document.write('<p>' + list[i].name + '  Height: ' + list[i].height)
-    if (list[i].height > 1.0) {
-      console.log ('  Wow that is a big Pokemon!' + '</p>')
-      document.write('  Wow that is a big Pokemon!' + '</p>')
-    };
-  };
-};
-*/
-// calls or executes function
-//printArrayDetails(pokemonList);
-//printArrayDetails(pokemonList2);
+});
